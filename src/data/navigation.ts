200d1d6f4498057726e6e6b7450602007654f5ee
mizzz（ずーみー） <mizzz._.dev@outlook.jp>
2026-07-13 18:38:53 +0900

    Portfolioをメインナビゲーションから除外

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -5,6 +5,5 @@ export const navigation = (locale: Locale) => [
   { key: 'home', href: localePath(locale, '/') },
   { key: 'profile', href: localePath(locale, '/profile') },
   { key: 'works', href: localePath(locale, '/works') },
-  { key: 'portfolio', href: localePath(locale, '/portfolio') },
   { key: 'blog', href: localePath(locale, '/blog') },
 ];
